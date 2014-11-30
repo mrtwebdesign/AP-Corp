93859d45db5705ddb6dafff3fc1704b7cbe54cd8
Matthew Taylor <matthew@mrtwebdesign.com>
2014-11-30 15:42:39 -0800

    added custom taxonomy

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ add_action( 'init', 'custom_post_types_init' );
  * @link http://codex.wordpress.org/Function_Reference/register_post_type
  */
 function custom_post_types_init() {
-	$labels = array(
+	$company_labels = array(
 		'name'               => _x( 'Companies', 'post type general name', 'apcorp' ),
 		'singular_name'      => _x( 'Company', 'post type singular name', 'apcorp' ),
 		'menu_name'          => _x( 'Companies', 'admin menu', 'apcorp' ),
@@ -47,8 +47,8 @@ function custom_post_types_init() {
 		'not_found_in_trash' => __( 'No companies found in Trash.', 'apcorp' )
 	);
 
-	$args = array(
-		'labels'             => $labels,
+	$company_args = array(
+		'labels'             => $company_labels,
 		'public'             => true,
 		'publicly_queryable' => true,
 		'show_ui'            => true,
@@ -58,12 +58,84 @@ function custom_post_types_init() {
 		'capability_type'    => 'post',
 		'has_archive'        => true,
 		'hierarchical'       => false,
-		'menu_position'      => null,
+		'menu_position'      => 21,
 		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'revisions' )
 	);
 
-	register_post_type( 'company', $args );
+	register_post_type( 'company', $company_args );
+
+	$product_line_labels = array(
+		'name'               => _x( 'Product Lines', 'post type general name', 'apcorp' ),
+		'singular_name'      => _x( 'Product Line', 'post type singular name', 'apcorp' ),
+		'menu_name'          => _x( 'Product Lines', 'admin menu', 'apcorp' ),
+		'name_admin_bar'     => _x( 'Product Line', 'add new on admin bar', 'apcorp' ),
+		'add_new'            => _x( 'Add New', 'Product Line', 'apcorp' ),
+		'add_new_item'       => __( 'Add New Product Line', 'apcorp' ),
+		'new_item'           => __( 'New Product Line', 'apcorp' ),
+		'edit_item'          => __( 'Edit Product Line', 'apcorp' ),
+		'view_item'          => __( 'View Product Line', 'apcorp' ),
+		'all_items'          => __( 'All Product Lines', 'apcorp' ),
+		'search_items'       => __( 'Search Product Lines', 'apcorp' ),
+		'parent_item_colon'  => __( 'Parent Product Lines:', 'apcorp' ),
+		'not_found'          => __( 'No product lines found.', 'apcorp' ),
+		'not_found_in_trash' => __( 'No product lines found in Trash.', 'apcorp' )
+	);
+
+	$product_line_args = array(
+		'labels'             => $product_line_labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'product-line' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 22,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'revisions' )
+	);
+
+	register_post_type( 'product-line', $product_line_args );
 }
+
+add_action( 'init', 'create_product_taxonomy' );
+
+function create_product_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Products', 'taxonomy general name' ),
+		'singular_name'              => _x( 'Product', 'taxonomy singular name' ),
+		'search_items'               => __( 'Search Products' ),
+		'popular_items'              => __( 'Popular Products' ),
+		'all_items'                  => __( 'All Products' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Product' ),
+		'update_item'                => __( 'Update Product' ),
+		'add_new_item'               => __( 'Add New Product' ),
+		'new_item_name'              => __( 'New Product Name' ),
+		'separate_items_with_commas' => __( 'Separate products with commas' ),
+		'add_or_remove_items'        => __( 'Add or remove products' ),
+		'choose_from_most_used'      => __( 'Choose from the most used products' ),
+		'not_found'                  => __( 'No products found.' ),
+		'menu_name'                  => __( 'Products' ),
+	);
+
+	$args = array(
+		'hierarchical'          => false,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'product' ),
+	);
+
+register_taxonomy( 'product', array( 'product-line', 'company'), $args );
+
+}
+
 
 if ( ! function_exists( 'apcorp_setup' ) ) :
 /**
@@ -91,7 +163,7 @@ function apcorp_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
